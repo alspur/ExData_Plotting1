d057817365620fd74e20fb5eed212f574d7daf80
alspur <alex.spurrier@gmail.com>
2014-05-07 21:38:28 -0400

    add plot1.r

---
+++ plot1.r
@@ -0,0 +1,26 @@
+# plot1.r
+
+# Load the data
+electric.data <- read.table("household_power_consumption.txt", sep = ";",
+                            na.strings = "?", header = TRUE, 
+                            colClasses = c("character", "character", "numeric", "numeric",
+                                           "numeric", "numeric", "numeric", "numeric",
+                                           "numeric"))
+
+#subset for Feb. 1-2, 2007
+date.idx <- grepl("^[1-2]\\/2\\/2007", electric.data$Date)
+electric.data <- electric.data[date.idx,]
+rownames(electric.data) <- NULL
+
+# Combine date and time columns
+electric.data$date.time <- paste(electric.data$Date, electric.data$Time)
+
+# Convert date.time column to POSIXct
+electric.data$date.time <- as.POSIXct(electric.data$date.time, format = "%d/%m/%Y %H:%M:%S")
+
+# Create plot1.png
+png(file = "plot1.png")
+hist(electric.data$Global_active_power, col = "red",
+     xlab = "Global Active Power (kilowatts)", 
+     main = "Global Active Power")
+dev.off()
